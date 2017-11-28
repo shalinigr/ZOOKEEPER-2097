caa09b785777857e270f11c9f9ad9c05dd04b1ad
Shalini <gundaiah@usc.edu>
2017-11-27 19:50:40 -0800

    edited the comment

--- src/java/main/org/apache/zookeeper/server/FinalRequestProcessor.java
+++ src/java/main/org/apache/zookeeper/server/FinalRequestProcessor.java
@@ -297,7 +297,7 @@ public class FinalRequestProcessor implements RequestProcessor {
             }
             case OpCode.exists: {
                 lastOp = "EXIS";
-                //Removal of TODO since this case works without any security check for existence of the node
+                //Removal of TODO since this case works without any check for existence of the node
                 ExistsRequest existsRequest = new ExistsRequest();
                 ByteBufferInputStream.byteBuffer2Record(request.request,
                         existsRequest);
